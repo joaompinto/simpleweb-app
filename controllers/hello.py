d401110685040a0412109e973c0ecbeca19cd96b
João Pinto <lamego.pinto@gmail.com>
2014-09-03 21:02:03 +0100

    Added view source with syntax highllight

--- controllers/hello.py
+++ controllers/hello.py
@@ -1,19 +1,10 @@
 ## -*- coding: utf-8 -*-
 from simpleweb import controller, template
 
-
 class HelloPage(object):
-
-    @controller.publish
-    def index(self):
-        return "Hello world!"
-
-
-class HelloTemplatePage(object):
 
     @controller.publish
     def index(self):
         return template.render('hello.html', word='world!')
 
 controller.attach('/hello/', HelloPage())
-controller.attach('/hello_using_template/', HelloTemplatePage())
--- controllers/view_source.py
+++ controllers/view_source.py
@@ -1,23 +1,39 @@
 ## -*- coding: utf-8 -*-
-from simpleweb import controller
-from os.path import join, exists, dirname
+from simpleweb import controller, template
+from pygments import highlight
+from pygments.lexers import PythonLexer, MakoHtmlLexer
+from pygments.formatters import HtmlFormatter
+from os.path import join, exists, dirname, basename
 import re
+
 
 class ViewSourcePage(object):
 
     @controller.publish
-    def default(sel, *args):
-        if len(args) != 1:
+    def default(self, *args):
+        source_name = 'index' if len(args) == 0 else args[0]
+        if not re.match('^([a-z_])+$', source_name):
             return
-        name = args[0]
-        if not re.match('^([a-z_])+$', name):
-            print "not match"
+        filename = join(dirname(__file__), source_name+'.py')
+        if not exists(filename):
             return
-        fname = join(dirname(__file__), name+'.py')
-        if exists(fname):
-            with open(fname) as f:
-                code = f.read()
-            return code
+
+        with open(filename) as file:
+            code = file.read()
+        source = '<b>controllers/%s</b>' % basename(filename)
+        source += highlight(code, PythonLexer(), HtmlFormatter(noclasses=True))
+
+        for source_name in re.findall("template.render\('([a-z_]*).", code):
+            filename = join(dirname(__file__), '..', 'views', source_name+'.html')
+            print filename
+            if exists(filename):
+                with open(filename) as file:
+                    code = file.read()
+                source += '<b>views/%s</b>' % basename(filename)
+                source += highlight(code, MakoHtmlLexer(), HtmlFormatter(noclasses=True))
+
+        controller.set_response('Content-Type', 'text/html')
+        return template.render('view_source.html', content=source)
 
 
 controller.attach('/view_source/', ViewSourcePage())
